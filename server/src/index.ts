faa11480e077d12631c804f25fbc0f815f511d8f
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-03-26 21:22:58 +0900

    Delete: entrypoint test

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,8 @@
 import './config/index';
 import express from 'express';
 import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-
-dotenv.config()
 
 const port: number = 4000;
 const app: any = express()
-
-console.log(process.env.MONGO_URL);
 
 app.listen(port, () => console.log(`Server listing on ${port}`));
