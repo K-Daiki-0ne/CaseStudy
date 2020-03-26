c8530b634a28d2584f5dbcd9f6eb7c144a5ad16a
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-03-26 21:18:59 +0900

    Test: dotenv

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,13 @@
 import './config/index';
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const port: number = 4000;
 const app: any = express()
+
+console.log(process.env.MONGO_URL);
 
 app.listen(port, () => console.log(`Server listing on ${port}`));
