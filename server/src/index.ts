c36edb39e3eca1654780744774a4a9d4b6013cb5
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-03-26 23:08:54 +0900

    Add: body-parser

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,4 +5,5 @@ import bodyParser from 'body-parser';
 const port: number = 4000;
 const app: any = express()
 
+app.use(bodyParser.json());
 app.listen(port, () => console.log(`Server listing on ${port}`));
