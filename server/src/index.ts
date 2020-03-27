def18f740c401974af4995d457130c448fce06b2
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-03-28 00:51:40 +0900

    Add: cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 import './config/db';
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import router from './controller/study';
 
 const port: number = 4000;
 const app: any = express()
 
 app.use(bodyParser.json());
-app.use('/', router)
+app.use(cors());
+app.use('/', router);
 app.listen(port, () => console.log(`Server listing on ${port}`));
