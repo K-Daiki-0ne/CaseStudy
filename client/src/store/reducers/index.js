918b9749940a785624a30fab7ceb98b15f5fee3c
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-03-29 01:49:08 +0900

    Setup: redux state management

---
+++ client/src/store/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import { postStudy } from '../reducers/postStudy';
+
+export const reducers = combineReducers({
+  postStudy
+})
--- client/src/store/state/index.js
+++ client/src/store/state/index.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { reducers } from '../reducers/index';
 
 export const store = createStore(
-  {},
+  reducers,
   compose(
     applyMiddleware(thunk)
   )
