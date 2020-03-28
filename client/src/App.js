26246c68455c0a57078f0aaa20336736ffd77660
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-03-28 23:49:33 +0900

    Delete: comment in index.js & fix return

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,4 @@
 import React from 'react';
 import { Study } from './view/Study';
 
-function App() {
-  return (
-    <div className="App">
-      <Study />
-    </div>
-  );
-}
-
-export default App;
+export const App = () => <Study />
