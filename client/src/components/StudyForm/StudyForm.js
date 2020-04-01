be22156759c32defcf1c96b5fea01255562b6b59
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-04-02 00:00:10 +0900

    Create: base StudyForm component

--- client/src/components/StudyForm/StudyForm.js
+++ client/src/components/StudyForm/StudyForm.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { TextField } from '@material-ui/core';
+
+const initValue = {
+  title: '',
+  detail: '',
+}
 
 export const StudyForm = (props) => {
+
+  const [values, setValued] = useState({});
   return (
-    <div>
-      Form StudyForm
-    </div>
+    <form autoComplete='off' noValidate>
+      <TextField 
+        name='title'  
+        variant='outlined'
+        label='Study title'
+        fullWidth
+        value={values.title}
+      />
+      <TextField 
+        name='detail'  
+        variant='outlined'
+        label='Study detail'
+        fullWidth
+        value={values.detail}
+      />
+    </form>
   )
 }
