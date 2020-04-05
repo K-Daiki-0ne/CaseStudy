a1a20ebdc36a1c3956f4788d34d40b1416bb6715
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-04-05 22:05:24 +0900

    Add: ButterTOast componenst

--- client/src/view/Study.js
+++ client/src/view/Study.js
@@ -2,12 +2,13 @@ import React from 'react';
 import StudyComponent from '../components/StudyComponent';
 import { StudyBar } from '../components/StudyBar/StudyBar';
 import { Container } from '@material-ui/core';
-
+import ButterToast, { POS_RIGHT,POS_TOP } from 'butter-toast';
 export const Study = () => {
   return (
     <Container maxWidth="lg">
       <StudyBar />
       <StudyComponent />
+      <ButterToast position={{ vertical: POS_TOP, horizontal: POS_RIGHT }} />
     </Container>
   )
 }
