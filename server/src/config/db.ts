ef9683d1ce599de9bdc5195837b57df4bff0efdc
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-04-06 22:57:44 +0900

    Add: useFindAdndModify

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -5,7 +5,7 @@ dotenv.config()
 
 const url: string = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/caseStudy';
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, 
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, 
   err => {
     if (!err) {
       console.log('DB connection ...Ok');
